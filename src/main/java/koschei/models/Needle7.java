13441bd1ff7910c31330c3fc37e4c775ccfc74e4
tda78 <tda78@mail.ru>
2022-09-02 03:09:18 +0700

    pp214

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private Deth8 deth8;
+    @Autowired
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
